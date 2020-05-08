8ec2f51b971b8144eb2f2c4419524a6cd8c20120
heyoo <yuuhe0913@gmail.com>
2020-05-09 04:21:41 +0900

    add main, search,store xml

---
+++ app/src/main/java/com/example/medicinebox/SearchSelect.java
@@ -0,0 +1,14 @@
+package com.example.medicinebox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_select);
+    }
+}
---
+++ app/src/main/java/com/example/medicinebox/StoreActivity.java
@@ -0,0 +1,14 @@
+package com.example.medicinebox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.store_activity);
+    }
+}
