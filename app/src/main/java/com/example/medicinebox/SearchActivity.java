49e05ced4729a59b59b49bda597ee0af9b2e687f
heyoo <yuuhe0913@gmail.com>
2020-05-22 03:49:40 +0900

    create medicine search - open api function

--- app/src/main/java/com/example/medicinebox/SearchActivity.java
+++ app/src/main/java/com/example/medicinebox/SearchActivity.java
@@ -3,10 +3,12 @@ package com.example.medicinebox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -21,6 +23,9 @@ import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -32,9 +37,7 @@ public class SearchActivity extends AppCompatActivity {
     TextView editSearch;
     ListView searchList;
 
-    boolean inname = false, initem = false;
-    String item_name = null;
-
+    List<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,30 +77,28 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
-
         // 메인에서 검색한 값 가져오기
         Intent intent = getIntent();
         final String word = intent.getExtras().getString("search");
         editSearch.setText(word);
 
-        final List<String> list = new ArrayList<>();
-
         AsyncTask.execute(new Runnable() {        // 비동기 방식으로 해야된됨. 안그럼 잘 안됨.
             @Override
             public void run() {
                 ArrayList<String> resultArray = new ArrayList<>();
+
                 try {
                     resultArray = search(word);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                //resultArray = search(word);
                 Log.i("resultArray", String.valueOf(resultArray));
                 int i;
                 for (i=0; i < resultArray.size(); i++) {
                     list.add(resultArray.get(i));
                 }
 
+                getXmlData(word);
 
                 SearchActivity.this.runOnUiThread(new Runnable() {                                       // UI 쓰레드에서 실행
                     @Override
@@ -114,7 +115,18 @@ public class SearchActivity extends AppCompatActivity {
                 }*/
             }
         });
+
+        searchList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(getApplicationContext(),list.get(position),Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), SearchSelect.class);
+                intent.putExtra("name",list.get(position)); //검색어 검색 결과화면으로 넘겨주기
+                startActivity(intent);
+            }
+        });
     }
+
 
 
     private ArrayList<String> search(String word) throws JSONException {
@@ -162,4 +174,69 @@ public class SearchActivity extends AppCompatActivity {
 
         return null;
     }
+
+    void getXmlData(String word){
+
+        StringBuffer buffer=new StringBuffer();
+
+        Log.i("word",word);
+        //String name = URLEncoder.encode(str);//한글의 경우 인식이 안되기에 utf-8 방식으로 encoding..
+        String key= "NNlPSgKdYOzfOQTW0r5g1EGKaq43lm%2FmEwfx%2Fyi09i6FtGJCPyG1fB2yIEvE4lCLfxs7X6O%2FXtZ89jslBzW%2BWw%3D%3D";
+        String queryUrl="http://apis.data.go.kr/1470000/MdcinGrnIdntfcInfoService/getMdcinGrnIdntfcInfoList?"+"ServiceKey="+key+"&item_name="+word;
+        Log.i("url",queryUrl);
+        try {
+            URL url= new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
+            InputStream is= url.openStream(); //url위치로 입력스트림 연결
+
+            XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
+            XmlPullParser xpp= factory.newPullParser();
+            xpp.setInput( new InputStreamReader(is, "UTF-8") ); //inputstream 으로부터 xml 입력받기
+
+            String tag;
+
+            xpp.next();
+            int eventType= xpp.getEventType();
+            Log.i("eventType", String.valueOf(eventType));
+
+            while( eventType != XmlPullParser.END_DOCUMENT ){
+                switch( eventType ){
+                    case XmlPullParser.START_DOCUMENT:
+                        buffer.append("파싱 시작...\n\n");
+                        break;
+
+                    case XmlPullParser.START_TAG:
+                        tag= xpp.getName();//테그 이름 얻어오기
+
+                        if(tag.equals("item")) ;// 첫번째 검색결과
+                        else if(tag.equals("ITEM_NAME")){
+                            buffer.append("품목명 : ");
+                            xpp.next();
+                            list.add(xpp.getText());
+                            buffer.append(xpp.getText());//category 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n");//줄바꿈 문자 추가
+                        }
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        tag= xpp.getName(); //테그 이름 얻어오기
+
+                        if(tag.equals("item")) buffer.append("\n");// 첫번째 검색결과종료..줄바꿈
+                        break;
+                }
+
+                eventType= xpp.next();
+            }
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch blocke.printStackTrace();
+            e.printStackTrace();
+        }
+
+        buffer.append("파싱 끝\n");
+        //return buffer.toString();//StringBuffer 문자열 객체 반환
+
+    }//getXmlData method....
 }
